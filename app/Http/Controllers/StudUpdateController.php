9f5baff34d1a450e6ededb328caf97f29c40493d
starvikas1988 <70792693+starvikas1988@users.noreply.github.com>
2024-02-24 21:52:11 +0530

    Update StudUpdateController.php
    update logic added.

--- app/Http/Controllers/StudUpdateController.php
+++ app/Http/Controllers/StudUpdateController.php
@@ -16,6 +16,19 @@ class StudUpdateController extends Controller
       $users = DB::select('select * from student where id = ?',[$id]);
       return view('stud_update',['users'=>$users]);
    }
+    public function update(Request $request, $id)
+    {
+        $name = $request->input('stud_name');
+        $email = $request->input('stud_email');
+        $subject = $request->input('stud_subject');
+        $message = $request->input('stud_message');
+        $datetime = $request->input('datetime');
+
+        DB::update('update student set stud_name = ?, stud_email = ?, stud_subject = ?, stud_message = ?, datetime = ? where id = ?', [$name, $email, $subject, $message, $datetime, $id]);
+
+        echo "Record updated successfully.<br/>";
+        echo '<a href="/edit-records">Click Here</a> to go back.';
+    }
 
     public function destroy($id) {
       DB::delete('delete from student where id = ?',[$id]);
