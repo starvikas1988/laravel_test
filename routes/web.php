0ea9693b8ec6204d0192599601277234dd8a5d2d
starvikas1988 <70792693+starvikas1988@users.noreply.github.com>
2024-02-24 22:09:57 +0530

    Update web.php
    added route for my edit form and update query

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,10 @@ Route::get('edit-records','StudUpdateController@index');
 Route::get('editRecords/{id}','StudUpdateController@show');
 Route::post('edit/{id}','StudUpdateController@edit');
 
+//vikas code starts
+Route::get('/edit/{id}', 'StudUpdateController@showEditForm');
+Route::post('/update/{id}', 'StudUpdateController@update');
+//vikas code ends
+
 Route::get('delete-records','StudUpdateController@index');
 Route::get('delete/{id}','StudUpdateController@destroy');
